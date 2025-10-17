0190f0b01c319fede3524bf38bf91f27ef52e62a
Yas-Mass <yasutoshi.g86@gmail.com>
2025-10-17 15:44:38 -0400

    Update R.precursor.Table1.R

--- R.precursor.Table1.R
+++ R.precursor.Table1.R
@@ -1,9 +1,10 @@
 # Table 1
 ##########
-
+rm(list=ls())
 
 # upload the dataset
 d1 <- Table1
+
 
 # density.epithelial.CD3CD4 START
 ######################################################################################################################
@@ -769,5 +770,6 @@ group2 <- d1_sub2$density.laminapropria.CD3CD8[d1_sub2$d.stage == "3serrated"]
 wilcox.test(group1, group2, alternative = "two.sided")
 ##########################################################################################################
 # density.laminapropria.CD3CD8 END
+
 
 
